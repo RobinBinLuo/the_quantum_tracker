dca920c56226bf737e1623d34b42c243a9830f80
RobinBinLuo <binluo0417@163.com>
2026-05-07 21:17:02 +0800

    add IonQ Q1 2026 operating loss details

--- shared.js
+++ shared.js
@@ -1,6 +1,6 @@
 export const DATA_PATH = "./data/companies.json";
 export const NEWS_PATH = "./data/news.json";
-const DATA_VERSION = "20260507-02";
+const DATA_VERSION = "20260507-03";
 const LANGUAGE_KEY = "quantum-frontier-language";
 
 const UI_TEXT = {
